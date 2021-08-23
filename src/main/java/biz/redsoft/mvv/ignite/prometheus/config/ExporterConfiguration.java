e9874aaa7fa974c2a99cdd30d0835a857a0ea623
javaronok <dmitry.gorchakov.murom@gmail.com>
2021-08-23 17:57:01 +0300

    Ignite SQL query metrics

--- src/main/java/biz/redsoft/mvv/ignite/prometheus/config/ExporterConfiguration.java
+++ src/main/java/biz/redsoft/mvv/ignite/prometheus/config/ExporterConfiguration.java
@@ -2,6 +2,7 @@ package biz.redsoft.mvv.ignite.prometheus.config;
 
 import biz.redsoft.mvv.ignite.prometheus.exporter.IgniteMetricExporter;
 import biz.redsoft.mvv.ignite.prometheus.metrics.IgniteNodeCollector;
+import biz.redsoft.mvv.ignite.prometheus.metrics.IgniteQueryMetricCollector;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +27,7 @@ public class ExporterConfiguration {
     IgniteMetricExporter exporter = new IgniteMetricExporter();
     Set<String> memoryRegionFilters = parseMemoryRegionFilters(igniteMemoryRegionFilter);
     exporter.register(new IgniteNodeCollector(igniteRestUrl, memoryRegionFilters));
+    exporter.register(new IgniteQueryMetricCollector(igniteRestUrl));
     return exporter;
   }
 
