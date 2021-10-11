72ff5b0c522fc496bd8c9c37c7720c3a815c13f4
javaronok <dmitry.gorchakov.murom@gmail.com>
2021-10-11 15:48:51 +0300

    Ignite SQL query metrics library

--- src/main/java/biz/redsoft/mvv/ignite/prometheus/config/ExporterConfiguration.java
+++ src/main/java/biz/redsoft/mvv/ignite/prometheus/config/ExporterConfiguration.java
@@ -3,12 +3,15 @@ package biz.redsoft.mvv.ignite.prometheus.config;
 import biz.redsoft.mvv.ignite.prometheus.exporter.IgniteMetricExporter;
 import biz.redsoft.mvv.ignite.prometheus.metrics.IgniteNodeCollector;
 import biz.redsoft.mvv.ignite.prometheus.metrics.IgniteQueryMetricCollector;
+import io.prometheus.client.Collector;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,12 +26,24 @@ public class ExporterConfiguration {
   private String igniteMemoryRegionFilter;
 
   @Bean
-  public IgniteMetricExporter metricExporter() {
+  public IgniteMetricExporter metricExporter(List<Collector> collectors) {
     IgniteMetricExporter exporter = new IgniteMetricExporter();
-    Set<String> memoryRegionFilters = parseMemoryRegionFilters(igniteMemoryRegionFilter);
-    exporter.register(new IgniteNodeCollector(igniteRestUrl, memoryRegionFilters));
-    exporter.register(new IgniteQueryMetricCollector(igniteRestUrl));
+    for (Collector collector : collectors)
+      exporter.register(collector);
     return exporter;
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "ignite.metrics.node.collector.enabled", havingValue = "true", matchIfMissing = true)
+  public IgniteNodeCollector nodeCollector() {
+    Set<String> memoryRegionFilters = parseMemoryRegionFilters(igniteMemoryRegionFilter);
+    return new IgniteNodeCollector(igniteRestUrl, memoryRegionFilters);
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "ignite.metrics.query.collector.enabled", havingValue = "true", matchIfMissing = true)
+  public IgniteQueryMetricCollector queryCollector() {
+    return new IgniteQueryMetricCollector(igniteRestUrl);
   }
 
   private static Set<String> parseMemoryRegionFilters(String igniteMemoryRegionFilter) {
