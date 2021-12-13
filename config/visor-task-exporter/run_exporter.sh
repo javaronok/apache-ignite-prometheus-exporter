2e0b48d900537fbf498228ceb258dadd72e27f9e
javaronok <dmitry.gorchakov.murom@gmail.com>
2021-12-13 13:31:22 +0300

    Configuration through visor-exporter and http-exporter

---
+++ config/visor-task-exporter/run_exporter.sh
@@ -0,0 +1,3 @@
+docker run -itd -p 9000:9000 --name apache-ignite-prometheus-exporter --net=host \
+ -e IGNITE_REST_URL="localhost:8080" -e IGNITE_REGION_FILTER="Default_Region" \
+ apache-ignite-prometheus-exporter
---
+++ config/visor-task-exporter/run_grafana.sh
@@ -0,0 +1,1 @@
+docker run -itd --name grafana -p 3000:3000 --net=host grafana/grafana
