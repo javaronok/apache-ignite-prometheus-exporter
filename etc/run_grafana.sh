b21335f6764823adbcff820e92c05c7768685652
javaronok <dmitry.gorchakov.murom@gmail.com>
2021-12-13 13:31:43 +0300

    Clean

--- etc/run_grafana.sh
+++
@@ -1,1 +0,0 @@
-docker run -itd --name grafana -p 3000:3000 --net=host grafana/grafana
--- etc/run_prometheus.sh
+++
@@ -1,1 +0,0 @@
-docker run -itd --name prometheus -p 9090:9090 -v /srv/prometheus/:/etc/prometheus --net=host prom/prometheus --config.file=/etc/prometheus/prometheus.yml
